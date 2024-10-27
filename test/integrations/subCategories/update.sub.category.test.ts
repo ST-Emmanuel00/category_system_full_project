90f74e29c6d9eec4884ee6ca60b639c879b1403d
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-10-27 01:21:55 -0500

    Fix test

--- test/integrations/subCategories/update.sub.category.test.ts
+++ test/integrations/subCategories/update.sub.category.test.ts
@@ -4,7 +4,7 @@ import { api } from "../../jest.setup";
 describe("Update sub category", () => {
     test("Update sub category success...[!]", async () => {
 
-        const subCategoryId = await db.subcategory.findFirst({
+        const subCategory = await db.subcategory.findFirst({
             orderBy: {
                 updatedAt: "desc",
             },
@@ -17,7 +17,11 @@ describe("Update sub category", () => {
 
         }
 
-        const response = await api.put(`/api/sub-categories/${subCategoryId!.id}`).send(data);
+        const response = await api.put(`/api/sub-categories/${subCategory!.id}`).send(data);
+
+        
+        console.log("response:", response.body)
+        console.log("subCategory:", subCategory)
 
 
         expect(response.status).toBe(200);
--- test/integrations/topics/remove.topic.test.ts
+++ test/integrations/topics/remove.topic.test.ts
@@ -3,15 +3,19 @@ import { api } from "../../jest.setup";
 
 describe("Remove topics", () => {
     test("Remove topics success...[!]", async () => {
-        const topicId = await db.topic.findFirst({
+        const topic = await db.topic.findFirst({
             orderBy: {
                 updatedAt: "desc",
             },
             select: { id: true }
         });
 
-        const response = await api.delete(`/api/topics/${topicId!.id}`);
+        const response = await api.delete(`/api/topics/${topic!.id}`);
 
+        console.log("response:", response.body)
+        console.log("topic:", topic)
+
+        
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty("info");
         expect(response.body).toHaveProperty("message");
