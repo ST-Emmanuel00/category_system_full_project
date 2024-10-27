470f752398cce24cb54d71e82c555f2791df2868
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-10-26 21:36:24 -0500

    Add validations to sub category and topic create funtion

--- src/services/subcategories/add.new.subcategory.service.ts
+++ src/services/subcategories/add.new.subcategory.service.ts
@@ -1,10 +1,17 @@
 import { db } from "../../config";
 import { AsyncCustomResponse, createSubcategory } from "../../types";
+import { AppError } from 'utils';
 
 export const addNewSubCategory = async (body: createSubcategory): AsyncCustomResponse => {
     try {
 
         const { name, description, categoryId } = body
+
+        const category = await db.category.findFirst({ where: { id: categoryId } })
+
+        if (!category) {
+            throw new AppError("Category not found.", 404)
+        }
         const newSubCategory = await db.subcategory.create({
             data: {
                 categoryId: categoryId,
--- src/services/topics/add.new.topic.service.ts
+++ src/services/topics/add.new.topic.service.ts
@@ -1,9 +1,16 @@
+import { AppError } from "utils";
 import { db } from "../../config";
 import { AsyncCustomResponse, createTopic,  } from "../../types";
 
 export const addNewTopic = async (body: createTopic): AsyncCustomResponse => {
     try {
         const { name, description, subcategoryId } = body
+
+        const subcategory = await db.subcategory.findFirst({ where: { id: subcategoryId } })
+
+        if (!subcategory) {
+            throw new AppError("Sub category not found.", 404)
+        }
         const newTopic = await db.topic.create({
             data: {
                 subcategoryId: subcategoryId,
